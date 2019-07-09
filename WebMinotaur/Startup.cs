fb0b6612dde59e680c1fc2057ff2aff89bcb75ca
Boubakar Dansoko <boubakar.dansoko@outlook.com>
2019-07-09 09:45:17 +0200

    Removed ForecastWeather class & servcie & DI

--- WebMinotaur/Startup.cs
+++ WebMinotaur/Startup.cs
@@ -32,11 +32,14 @@ namespace WebMinotaur
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDefaultIdentity<AppUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddRazorPages();
+
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
