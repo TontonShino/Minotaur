a1fa7d85acd10167fe870e65aa2e9c7bfb473525
TontonShino <bdansoko@gmail.com>
2019-07-24 00:02:52 +0200

    adding UserInfo

---
+++ SharedLib/Models/UserInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLib.Models
+{
+    public class UserInfo
+    {
+        public string Username { get; set; }
+        public string UserId { get; set; }
+    }
+}
