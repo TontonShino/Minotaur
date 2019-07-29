bb9bf109d0ff9e349fd0c6ed378b16623af8c708
Boubakar Dansoko <boubakar.dansoko@outlook.com>
2019-07-29 19:01:58 +0200

    login route request token

--- WebMinotaur/Controllers/DevicesController.cs
+++ WebMinotaur/Controllers/DevicesController.cs
@@ -12,114 +12,128 @@ using SharedLib.IRepositories;
 using WebMinotaur.Data;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.Cookies;
-
+using SharedLib.Models;
 
 namespace WebMinotaur.Controllers
 {
-    [Authorize(AuthenticationSchemes = "Bearer,Identity.Application")]
-    //[Authorize]
+    //[Authorize(AuthenticationSchemes = "Bearer,Identity.Application")]
+    
     [Route("api/[controller]")]
     [ApiController]
     public class DevicesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        private readonly IDevicesRepository _dr; 
-        public DevicesController(ApplicationDbContext context, IDevicesRepository dr)
+        private readonly IDevicesRepository _dr;
+        private readonly IAppUserTokensRepository appUserTokensRepository;
+        public DevicesController(ApplicationDbContext context, IDevicesRepository dr, IAppUserTokensRepository appUserTokensRepository)
         {
             _context = context;
             _dr = dr;
+            this.appUserTokensRepository = appUserTokensRepository;
            
         }
+        #region Old code
+        ////[HttpGet]
+        ////[Route("userdevices/{id}")]
+        ////public async Task<ActionResult<List<Device>>> GetUserDevices(string id)
+        ////{
+        ////    var at = Request.Headers["Authorization"];
+        ////    return await _dr.GetDevicesAsyncByUserId(id);
+        ////}
+
+        //// GET: api/Devices
         //[HttpGet]
         //[Route("userdevices/{id}")]
-        //public async Task<ActionResult<List<Device>>> GetUserDevices(string id)
+        //public async Task<ActionResult<List<Device>>> GetDevices(string id)
         //{
-        //    var at = Request.Headers["Authorization"];
+        //    Console.WriteLine("ID entered {id}");
         //    return await _dr.GetDevicesAsyncByUserId(id);
         //}
 
-        // GET: api/Devices
-        [HttpGet]
-        [Route("userdevices/{id}")]
-        public async Task<ActionResult<List<Device>>> GetDevices(string id)
-        {
-            Console.WriteLine("ID entered {id}");
-            return await _dr.GetDevicesAsyncByUserId(id);
-        }
+        //// GET: api/Devices/5
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult<Device>> GetDevice(string id)
+        //{
+        //    var device = await _context.Devices.FindAsync(id);
 
-        // GET: api/Devices/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Device>> GetDevice(string id)
-        {
-            var device = await _context.Devices.FindAsync(id);
+        //    if (device == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (device == null)
-            {
-                return NotFound();
-            }
+        //    return device;
+        //}
 
-            return device;
-        }
+        //// PUT: api/Devices/5
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutDevice(string id, Device device)
+        //{
+        //    if (id != device.Id)
+        //    {
+        //        return BadRequest();
+        //    }
 
-        // PUT: api/Devices/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutDevice(string id, Device device)
-        {
-            if (id != device.Id)
-            {
-                return BadRequest();
-            }
+        //    _context.Entry(device).State = EntityState.Modified;
 
-            _context.Entry(device).State = EntityState.Modified;
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!DeviceExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!DeviceExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    return NoContent();
+        //}
 
-            return NoContent();
-        }
+        //// POST: api/Devices
+        //[HttpPost]
+        //public async Task<ActionResult<Device>> PostDevice(Device device)
+        //{
+        //    _context.Devices.Add(device);
+        //    await _context.SaveChangesAsync();
 
-        // POST: api/Devices
+        //    return CreatedAtAction("GetDevice", new { id = device.Id }, device);
+        //}
+
+        //// DELETE: api/Devices/5
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult<Device>> DeleteDevice(int id)
+        //{
+        //    var device = await _context.Devices.FindAsync(id);
+        //    if (device == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Devices.Remove(device);
+        //    await _context.SaveChangesAsync();
+
+        //    return device;
+        //}
+
+        //private bool DeviceExists(string id)
+        //{
+        //    return _context.Devices.Any(e => e.Id == id);
+        //}
+        #endregion oldCode
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         [HttpPost]
-        public async Task<ActionResult<Device>> PostDevice(Device device)
+        [Route("NewDevice")]
+        public Device NewDevice([FromBody]DeviceModel deviceModel)
         {
-            _context.Devices.Add(device);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetDevice", new { id = device.Id }, device);
-        }
-
-        // DELETE: api/Devices/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Device>> DeleteDevice(int id)
-        {
-            var device = await _context.Devices.FindAsync(id);
-            if (device == null)
-            {
-                return NotFound();
-            }
-
-            _context.Devices.Remove(device);
-            await _context.SaveChangesAsync();
-
-            return device;
-        }
-
-        private bool DeviceExists(string id)
-        {
-            return _context.Devices.Any(e => e.Id == id);
+            //get Token
+            var accessToken = Request.Headers["Authorization"];
+            Console.WriteLine(accessToken);
+            return new Device();
         }
     }
 }
