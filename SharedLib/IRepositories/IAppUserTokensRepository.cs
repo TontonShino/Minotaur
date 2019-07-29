c2d374e59f32ece98701ba1549de01308e239415
Boubakar Dansoko <boubakar.dansoko@outlook.com>
2019-07-29 18:56:09 +0200

    Adding AppUserToken Interface & implementation

---
+++ SharedLib/IRepositories/IAppUserTokensRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLib.IRepositories
+{
+    public interface IAppUserTokensRepository
+    {
+        AppUserToken Create(AppUserToken appUserToken);
+        AppUserToken Get(string id);
+        AppUserToken Update(AppUserToken appUserToken);
+        void Delete(string id);
+
+    }
+}
---
+++ WebMinotaur/Repositories/AppUserTokensRepository.cs
@@ -0,0 +1,40 @@
+﻿using SharedLib;
+using SharedLib.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMinotaur.Data;
+
+namespace WebMinotaur.Repositories
+{
+    public class AppUserTokensRepository : IAppUserTokensRepository
+    {
+        private readonly ApplicationDbContext db;
+        public AppUserTokensRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public AppUserToken Create(AppUserToken appUserToken)
+        {
+            db.AppUserTokens.Add(appUserToken);
+            db.SaveChanges();
+            return appUserToken;
+        }
+
+        public void Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppUserToken Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AppUserToken Update(AppUserToken appUserToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
