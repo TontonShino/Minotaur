51028d47f46ab01f0699cf0617b2a71f92e54827
TontonShino <bdansoko@gmail.com>
2019-07-29 22:10:48 +0200

    adding IAppUserRepository & implementation

---
+++ SharedLib/IRepositories/IAppUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLib.IRepositories
+{
+    public interface IAppUserRepository
+    {
+        AppUser Get(string id);
+        AppUser GetByUsername(string username);
+    }
+}
---
+++ WebMinotaur/Repositories/AppUserRepository.cs
@@ -0,0 +1,28 @@
+﻿using SharedLib;
+using SharedLib.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMinotaur.Data;
+
+namespace WebMinotaur.Repositories
+{
+    public class AppUserRepository : IAppUserRepository
+    {
+        private readonly ApplicationDbContext db;
+        public AppUserRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public AppUser Get(string id)
+        {
+            return db.AppUsers.FirstOrDefault(u => u.Id == id);
+        }
+
+        public AppUser GetByUsername(string username)
+        {
+            return db.AppUsers.FirstOrDefault(u => u.UserName == username);
+        }
+    }
+}
