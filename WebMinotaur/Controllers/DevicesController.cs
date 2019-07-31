33ce6b0dc69484724d2cb70e832c7b6f8e12c68f
Boubakar Dansoko <boubakar.dansoko@outlook.com>
2019-07-31 17:23:00 +0200

    Adding Post Method & extract token from header devicesController [API]

--- WebMinotaur/Controllers/DevicesController.cs
+++ WebMinotaur/Controllers/DevicesController.cs
@@ -2,14 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SharedLib;
+using SharedLib.IRepositories;
+using SharedLib.Models;
 
 namespace WebMinotaur.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class DevicesController : ControllerBase
     {
+        private readonly IDevicesRepository devicesRepository;
+        private readonly IAppUserTokensRepository appUserTokensRepository;
+        private string accessTokenHeader;
+        public DevicesController
+            (IDevicesRepository devicesRepository,
+            IAppUserTokensRepository appUserTokensRepository
+            )
+        {
+            this.devicesRepository = devicesRepository;
+            this.appUserTokensRepository = appUserTokensRepository;
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost]
+        public ActionResult<Device> PostDevice([FromBody]DeviceModel deviceModel)
+        {
+            ExtractToken();
+
+            if(accessTokenHeader != null)
+            {
+                var token = appUserTokensRepository.Get(accessTokenHeader);
+                if(token != null && token.Enabled == true)
+                {
+                    var device = new Device
+                    {
+                        AppUserId = token.AppUserId,
+                        Name = deviceModel.Name,
+                        Description = deviceModel.Description
+                    };
+
+                    devicesRepository.AddDevice(device);
+                    return device;
+                }
+            }
+
+            return Unauthorized();
+        }
+        private void ExtractToken()
+        {
+            accessTokenHeader = Request.Headers["Authorization"].ToString().Replace("Bearer", "").Trim();
+        }
     }
 }
