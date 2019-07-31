a0febaada433a82dd983e0b690d0edd90cca2a43
Boubakar Dansoko <boubakar.dansoko@outlook.com>
2019-07-31 17:23:49 +0200

    adding post method for InfoIp Controller & extract token in Header

--- WebMinotaur/Controllers/InfoIpController.cs
+++ WebMinotaur/Controllers/InfoIpController.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SharedLib;
+using SharedLib.IRepositories;
+using SharedLib.Models;
 
 namespace WebMinotaur.Controllers
 {
@@ -11,5 +16,41 @@ namespace WebMinotaur.Controllers
     [ApiController]
     public class InfoIpController : ControllerBase
     {
+        private readonly IInfoIpRepository infoIpRepository;
+        private readonly IAppUserTokensRepository appUserTokensRepository;
+        private string accessTokenHeader;
+        public InfoIpController
+            (IInfoIpRepository infoIpRepository,
+            IAppUserTokensRepository appUserTokensRepository)
+        {
+            this.infoIpRepository = infoIpRepository;
+            this.appUserTokensRepository = appUserTokensRepository;
+        }
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost]
+        public ActionResult<InfoIP> PostInfoIp([FromBody]InfoIpModel infoIpModel)
+        {
+            ExtractToken();
+            if(accessTokenHeader != null)
+            {
+                var token = appUserTokensRepository.Get(accessTokenHeader);
+                if(token != null && token.Enabled)
+                {
+                    var infoIp = new InfoIP
+                    {
+                        DeviceId = infoIpModel.deviceId,
+                        Ip = infoIpModel.ip,
+                        Record = DateTime.UtcNow
+                    };
+                    infoIpRepository.Create(infoIp);
+                    return infoIp;
+                }
+            }
+            return Unauthorized();
+        }
+        private void ExtractToken()
+        {
+            accessTokenHeader = Request.Headers["Authorization"].ToString().Replace("Bearer", "").Trim();
+        }
     }
 }
