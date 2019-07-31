7964600733f92518f89370f2715aa5339d81797c
Boubakar Dansoko <boubakar.dansoko@outlook.com>
2019-07-31 17:21:20 +0200

    adding InfoIp repo & interface

---
+++ SharedLib/IRepositories/IInfoIpRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLib.IRepositories
+{
+    public interface IInfoIpRepository
+    {
+        InfoIP Get(int id);
+        Task<InfoIP> GetAsync(int id);
+        InfoIP Create(InfoIP infoIP);
+        Task<InfoIP> CreateAsync(InfoIP infoIP);
+
+
+    }
+}
---
+++ WebMinotaur/Repositories/InfoIpRepository.cs
@@ -0,0 +1,42 @@
+﻿using SharedLib;
+using SharedLib.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMinotaur.Data;
+
+namespace WebMinotaur.Repositories
+{
+    public class InfoIpRepository : IInfoIpRepository
+    {
+        private readonly ApplicationDbContext db;
+        public InfoIpRepository(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public InfoIP Create(InfoIP infoIP)
+        {
+            db.InfoIP.Add(infoIP);
+            db.SaveChanges();
+            return infoIP;
+        }
+
+        public async Task<InfoIP> CreateAsync(InfoIP infoIP)
+        {
+            db.InfoIP.Add(infoIP);
+            await db.SaveChangesAsync();
+            return infoIP;
+        }
+
+        public InfoIP Get(int id)
+        {
+            return db.InfoIP.Find(id);
+        }
+
+        public async Task<InfoIP> GetAsync(int id)
+        {
+            return await db.InfoIP.FindAsync(id);
+        }
+    }
+}
