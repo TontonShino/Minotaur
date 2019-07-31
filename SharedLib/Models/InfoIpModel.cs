156afe0a478a873d69bf5201205547290797d5e9
Boubakar Dansoko <boubakar.dansoko@outlook.com>
2019-07-31 17:21:52 +0200

    adding InfoIP model

---
+++ SharedLib/Models/InfoIpModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedLib.Models
+{
+    public class InfoIpModel
+    {
+        public string ip { get; set; }
+        public string deviceId { get; set; }
+    }
+}
