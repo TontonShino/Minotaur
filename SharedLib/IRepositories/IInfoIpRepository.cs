0df905027c9ad5829d114b7bda62da4b77fb937b
TontonShino <bdansoko@gmail.com>
2019-08-03 21:59:28 +0200

    adding CRUD Methods on interface & implements

--- SharedLib/IRepositories/IInfoIpRepository.cs
+++ SharedLib/IRepositories/IInfoIpRepository.cs
@@ -11,6 +11,12 @@ namespace SharedLib.IRepositories
         Task<InfoIP> GetAsync(int id);
         InfoIP Create(InfoIP infoIP);
         Task<InfoIP> CreateAsync(InfoIP infoIP);
+        List<InfoIP> GetLast(string deviceId);
+        Task<List<InfoIP>> GetLastAsync(string deviceId);
+        List<InfoIP> GetByDeviceId(string deviceId);
+        Task<List<InfoIP>> GetByDeviceIdAsync(string deviceId);
+        void Delete(int id);
+        Task DeleteAsync(int id);
 
 
     }
--- WebMinotaur/Repositories/InfoIpRepository.cs
+++ WebMinotaur/Repositories/InfoIpRepository.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebMinotaur.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebMinotaur.Repositories
 {
@@ -29,6 +30,20 @@ namespace WebMinotaur.Repositories
             return infoIP;
         }
 
+        public void Delete(int id)
+        {
+            var infoIp = db.InfoIP.Find(id);
+            db.InfoIP.Remove(infoIp);
+            db.SaveChanges();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var infoIp = await db.InfoIP.FindAsync(id);
+            db.InfoIP.Remove(infoIp);
+            await db.SaveChangesAsync();
+        }
+
         public InfoIP Get(int id)
         {
             return db.InfoIP.Find(id);
@@ -37,6 +52,28 @@ namespace WebMinotaur.Repositories
         public async Task<InfoIP> GetAsync(int id)
         {
             return await db.InfoIP.FindAsync(id);
+        }
+
+        public List<InfoIP> GetByDeviceId(string deviceId)
+        {
+            return db.InfoIP.Where(d => d.DeviceId == deviceId).ToList();
+        }
+
+        public async Task<List<InfoIP>> GetByDeviceIdAsync(string deviceId)
+        {
+            return await db.InfoIP.Where(d => d.DeviceId == deviceId).ToListAsync();
+        }
+
+        public List<InfoIP> GetLast(string deviceId)
+        {
+            //Todo: test if really 10 last
+            return db.InfoIP.Where(d => d.DeviceId == deviceId).TakeLast(10).ToList();
+        }
+
+        public async Task<List<InfoIP>> GetLastAsync(string deviceId)
+        {
+            //Todo: test if really 10 last
+            return await db.InfoIP.Where(d => d.DeviceId == deviceId).TakeLast(10).ToListAsync();
         }
     }
 }
