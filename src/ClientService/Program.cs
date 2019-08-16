7f811188f6cf6fb4e141895c14a56d440f1f28d3
TontonShino <bdansoko@gmail.com>
2019-08-16 17:45:56 +0200

    adding IoC Request / DeviceConfiguration / JsonManager

--- src/ClientService/Program.cs
+++ src/ClientService/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClientService.IService;
+using ClientService.Service;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -18,6 +20,9 @@ namespace ClientService
             Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
+                    services.AddSingleton<IRequest, Request>();
+                    services.AddSingleton<IDeviceConfiguration, DeviceConfiguration>();
+                    services.AddSingleton<IJsonManager, JsonManager>();
                     services.AddHostedService<Worker>();
                 });
     }
